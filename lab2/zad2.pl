d72f86db1a418c739463ac5035ae139601d2e38f
Kacper Dęga <127394561+KacperDega@users.noreply.github.com>
2024-04-03 16:35:09 +0200

    Create zad2.pl

---
+++ lab2/zad2.pl
@@ -0,0 +1,67 @@
+osoba(jan).
+osoba(pawel).
+osoba(adrian).
+osoba(darek).
+osoba(tomek).
+osoba(olek).
+osoba(maria).
+osoba(anna).
+osoba(magda).
+osoba(ilona).
+osoba(patrycja).
+osoba(marek).
+
+mezczyzna(jan).
+mezczyzna(pawel).
+mezczyzna(adrian).
+mezczyzna(darek).
+mezczyzna(tomek).
+mezczyzna(olek).
+mezczyzna(marek).
+
+rodzic(darek,maria).
+rodzic(darek,jan).
+rodzic(ilona,maria).
+rodzic(ilona,jan).
+rodzic(tomek,olek).
+rodzic(maria,olek).
+rodzic(jan,pawel).
+rodzic(anna,pawel).
+
+
+rodzic(adrian,magda).
+rodzic(patrycja,magda).
+rodzic(patrycja, marek).
+rodzic(olek, marek).
+
+kobieta(X) :-
+    osoba(X),
+    \+mezczyzna(X).
+
+ojciec(X,Y) :-
+    rodzic(X,Y),
+    mezczyzna(X).
+
+matka(X,Y) :-
+    rodzic(X,Y),
+    kobieta(X).
+
+corka(X,Y) :-
+    kobieta(X),
+    rodzic(Y,X).
+
+brat_rodzony(X,Y) :-
+    mezczyzna(X),
+    X \= Y ,
+    (   matka(M,X), matka(M,Y) 	),
+    (   ojciec(O,X), ojciec(O,Y) 	).
+    
+brat_przyrodni(X,Y) :-
+    mezczyzna(X),
+    X \= Y,
+    (
+    	(   (   matka(M,X), \+matka(M,Y)),	(   ojciec(O,X), ojciec(O,Y)	)	);
+    	(   (   matka(M,X), matka(M,Y)	),	(   ojciec(O,X), \+ojciec(O,Y)	)	)
+    ).
+    
+kuzyn(X,Y) :-
